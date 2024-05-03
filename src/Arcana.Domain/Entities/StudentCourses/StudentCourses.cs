f2a8a8a2619ba1271cda02cb4a36a4c227ba4320
Diyorbek <muhammadjonovd793@gmail.com>
2024-05-03 19:32:32 +0500

    Create Student Course Model

---
+++ src/Arcana.Domain/Entities/StudentCourses/StudentCourses.cs
@@ -0,0 +1,16 @@
+﻿using Arcana.Domain.Commons;
+using Arcana.Domain.Entities.Courses;
+using Arcana.Domain.Entities.Students;
+using Arcana.Domain.Entities.Instructors;
+
+namespace Arcana.Domain.Entities.StudentCourses;
+
+public class StudentCourses : Auditable
+{
+    public long StudentId { get; set; }
+    public Student Student { get; set; }
+    public long CourseId { get; set; }  
+    public Course Course { get; set; }
+    public long InstructorId {  get; set; }
+    public Instructor Instructor { get; set; }
+}
---
+++ src/Arcana.WebApi/Models/StudentCourses/StudentCourseUpdateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Arcana.WebApi.Models.StudentCourses;
+
+public class StudentCourseUpdateModel
+{
+    public long CourseId { get; set; }
+    public long StudentId { get; set; }
+    public long InstructorId { get; set; }
+}
