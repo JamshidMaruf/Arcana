312c76355021d9ffe845f4945cffb10559369138
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-05-03 18:31:16 +0500

    Lesson and LessonComments are ready

---
+++ src/Arcana.WebApi/Models/LessonComments/LessonCommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Arcana.Domain.Commons;
+using Arcana.Domain.Entities.Instructors;
+using Arcana.Domain.Entities.Lessons;
+using Arcana.Domain.Entities.Users;
+
+namespace Arcana.WebApi.Models.LessonComments;
+
+public class LessonCommentViewModel : Auditable
+{
+    public long Id { get; set; }
+    public string Content { get; set; }
+    public Lesson Lesson { get; set; }
+    public User User { get; set; }
+    public Instructor Instructor { get; set; }
+    public LessonComment Parent { get; set; }
+}
---
+++ src/Arcana.WebApi/Models/Lessons/LessonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Arcana.Domain.Entities.Commons;
+
+namespace Arcana.WebApi.Models.Lessons;
+
+public class LessonViewModel
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Asset File { get; set; }
+    //public Module Module { get; set; }
+}
