903c96484f398fca8c1602ffde94d8888c0c2f27
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-05-03 06:06:20 -0700

    Update CourseCreateModel.cs

--- src/Arcana.WebApi/Models/Courses/CourseCreateModel.cs
+++ src/Arcana.WebApi/Models/Courses/CourseCreateModel.cs
@@ -7,11 +7,11 @@ public class CourseCreateModel
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public DateTime Duration { get; set; }
+    public int Duration { get; set; }
     public int CountOfLessons { get; set; }
     public Level Level { get; set; }
     public long CategoryId { get; set; }
     public long InstructorId { get; set; }
     public long FileId { get; set; }
-    //public long LanguageId {  get; set; }
+    public long LanguageId { get; set; }
 }
