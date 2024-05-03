52300519b0deb08988e4c1c15afc7633eb343419
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-03 19:01:44 +0500

    Added CourseModul DTOs

---
+++ src/Arcana.WebApi/Models/CourseModules/CourseModulCreateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Arcana.WebApi.Models.CourseModules;
+
+public class CourseModulCreateModel
+{
+    public string Name { get; set; }
+    public long CourseId { get; set; }
+}
---
+++ src/Arcana.WebApi/Models/CourseModules/CourseModulViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Arcana.WebApi.Models.CourseModules;
+
+public class CourseModulViewModel
+{
+    public string Name { get; set; }
+    public CourseModulViewModel Course { get; set; }
+}
