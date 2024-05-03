4e076211740fb31807b61fe611bab7f8be4316ef
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-05-03 16:37:34 +0500

    Created Course and CourseCategory Model

---
+++ src/Arcana.WebApi/Models/Courses/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Arcana.Domain.Enums.Levels;
+
+namespace Arcana.WebApi.Models.Courses;
+
+public class CourseViewModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public DateTime Duration { get; set; }
+    public int Count_Of_Lessons { get; set; }
+    public Level Level { get; set; }
+    public long Category_Id { get; set; }
+    public long Instructor_Id { get; set; }
+    public long File_Id { get; set; }
+    //public long Language_Id {  get; set; }
+}
