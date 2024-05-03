2d72b33b6234ee04822f04907b84eac81ed50896
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-03 17:36:24 +0500

    Added CourseComment entity and models

---
+++ src/Arcana.Domain/Entities/CourseComments/CourseComment.cs
@@ -0,0 +1,13 @@
+﻿using Arcana.Domain.Commons;
+using Arcana.Domain.Entities.Students;
+
+namespace Arcana.Domain.Entities.CourseComments;
+
+public class CourseComment : Auditable
+{
+    public long StudentId { get; set; }
+    public Student Student { get; set; }
+    public long CourseId { get; set; }
+    // public Course Course { get; set; }
+    public string Content { get; set; }
+}
