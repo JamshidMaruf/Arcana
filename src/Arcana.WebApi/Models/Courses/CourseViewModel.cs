aaf520ef97eb447b63a254ef6be81acc298b3e75
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-05-03 16:43:55 +0500

    Update CourseViewModel.cs

--- src/Arcana.WebApi/Models/Courses/CourseViewModel.cs
+++ src/Arcana.WebApi/Models/Courses/CourseViewModel.cs
@@ -9,10 +9,10 @@ public class CourseViewModel
     public string Description { get; set; }
     public decimal Price { get; set; }
     public DateTime Duration { get; set; }
-    public int Count_Of_Lessons { get; set; }
+    public int CountOfLessons { get; set; }
     public Level Level { get; set; }
-    public long Category_Id { get; set; }
-    public long Instructor_Id { get; set; }
-    public long File_Id { get; set; }
-    //public long Language_Id {  get; set; }
+    // public CategoryViewModel Category { get; set; }
+    // public InstructorViewModel Instructor { get; set; }
+    // public AssetViewModel File { get; set; }
+    //public LanguageViewModel Language {  get; set; }
 }
