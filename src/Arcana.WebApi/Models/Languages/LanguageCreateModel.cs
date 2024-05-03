cf55b32ff7c95846891efd649064e4f329190a1e
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-03 16:07:10 +0500

    Added Language Entity and Models

---
+++ src/Arcana.WebApi/Models/Languages/LanguageCreateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Arcana.WebApi.Models.Languages;
+
+public class LanguageCreateModel
+{
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+}
---
+++ src/Arcana.WebApi/Models/Languages/LanguageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Arcana.WebApi.Models.Languages;
+
+public class LanguageViewModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+}
