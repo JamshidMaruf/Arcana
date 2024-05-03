63d797963167259217057bfcfba40da7c24f30a2
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-05-03 18:12:08 +0500

    Create QuizQuestion and Question models

---
+++ src/Arcana.WebApi/Models/QuizQuestions/QuizQuestionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Arcana.Domain.Entities.Questions;
+using Arcana.WebApi.Models.Questions;
+
+namespace Arcana.WebApi.Models.QuizQuestions;
+
+public class QuizQuestionViewModel
+{
+    public long Id { get; set; }
+    //public QuizViewModel Quiz { get; set; }
+    public QuestionViewModel Question { get; set; }
+}
