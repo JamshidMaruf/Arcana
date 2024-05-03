e449181dadb51ae81e81e238b55f1ca7bd612380
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-05-03 08:16:21 -0700

    Update StudentCourses.cs

--- src/Arcana.Domain/Entities/StudentCourses/StudentCourses.cs
+++ src/Arcana.Domain/Entities/StudentCourses/StudentCourses.cs
@@ -8,9 +8,10 @@ namespace Arcana.Domain.Entities.StudentCourses;
 public class StudentCourses : Auditable
 {
     public long StudentId { get; set; }
-    public Student Student { get; set; }
     public long CourseId { get; set; }  
-    public Course Course { get; set; }
     public long InstructorId {  get; set; }
+    
+    public Student Student { get; set; }
+    public Course Course { get; set; }
     public Instructor Instructor { get; set; }
 }
