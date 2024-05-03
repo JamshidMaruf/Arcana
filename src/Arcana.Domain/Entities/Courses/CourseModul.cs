64861d500c48856b219396002d63fe62298b30e8
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-03 18:58:14 +0500

    Added InstructorStars and CourseModul Entities

---
+++ src/Arcana.Domain/Entities/Courses/CourseModul.cs
@@ -0,0 +1,11 @@
+﻿using Arcana.Domain.Commons;
+
+namespace Arcana.Domain.Entities.Courses;
+
+public class CourseModul : Auditable
+{
+    public string Name { get; set; }
+    public long CourseId { get; set; }
+
+    public Course Course { get; set;}
+}
