b18140f68cc97541af85c52f1301f7d9084dec51
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-05-03 07:46:47 -0700

    Added Id to InstructorStarsViewModel.cs

--- src/Arcana.WebApi/Models/InstructorStars/InstructorStarsViewModel.cs
+++ src/Arcana.WebApi/Models/InstructorStars/InstructorStarsViewModel.cs
@@ -5,6 +5,7 @@ namespace Arcana.WebApi.Models.InstructorStars;
 
 public class InstructorStarsViewModel
 {
+    public long Id { get; set; }
     public StudentViewModel Student { get; set; }
     public InstructorViewModel Instructor { get; set; }
     public byte Stars { get; set; }
