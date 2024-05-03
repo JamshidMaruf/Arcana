72c4b3621708ad7256c8be0bc3e9bb40252b139b
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-03 19:08:47 +0500

    Added InstructorStars DTOs

---
+++ src/Arcana.WebApi/Models/InstructorStars/InstructorStarsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Arcana.WebApi.Models.Students;
+using Arcana.WebApi.Models.Instructors;
+
+namespace Arcana.WebApi.Models.InstructorStars;
+
+public class InstructorStarsViewModel
+{
+    public StudentViewModel Student { get; set; }
+    public InstructorViewModel Instructor { get; set; }
+    public byte Stars { get; set; }
+}
