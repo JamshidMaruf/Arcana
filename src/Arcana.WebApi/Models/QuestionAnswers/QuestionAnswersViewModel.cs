7d9d796096a55edc427afb10760136490c52fa9d
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-05-03 17:29:17 +0500

    Update QuestionAnswersViewModel.cs

--- src/Arcana.WebApi/Models/QuestionAnswers/QuestionAnswersViewModel.cs
+++ src/Arcana.WebApi/Models/QuestionAnswers/QuestionAnswersViewModel.cs
@@ -5,4 +5,5 @@ public class QuestionAnswersViewModel
     public long Id { get; set; }
     public string Content { get; set; }
     //public QuestionViewModel Question { get; set; }
+    public bool IsCorrect { get; set; }
 }
