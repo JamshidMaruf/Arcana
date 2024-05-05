2600b4da6d726cd07d6cda09032ba09f54cf0838
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-05 17:10:57 +0500

    Update CourseModule

--- src/Arcana.Domain/Entities/Courses/CourseModule.cs
+++ src/Arcana.Domain/Entities/Courses/CourseModule.cs
@@ -2,7 +2,7 @@
 
 namespace Arcana.Domain.Entities.Courses;
 
-public class CourseModul : Auditable
+public class CourseModule : Auditable
 {
     public string Name { get; set; }
     public long CourseId { get; set; }
