0f21809d7307e0bd763c782f170090860581a581
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-05-05 22:45:27 +0500

    made entities nullable

--- src/Arcana.WebApi/Models/LessonComments/LessonCommentCreateModel.cs
+++ src/Arcana.WebApi/Models/LessonComments/LessonCommentCreateModel.cs
@@ -5,8 +5,8 @@ namespace Arcana.WebApi.Models.LessonComments;
 public class LessonCommentCreateModel : Auditable
 {
     public long LessonId { get; set; }
-    public long UserId { get; set; }
-    public long InstructorId { get; set; }
+    public long? UserId { get; set; }
+    public long? InstructorId { get; set; }
     public long? ParentId { get; set; }
     public string Content { get; set; }
 }
