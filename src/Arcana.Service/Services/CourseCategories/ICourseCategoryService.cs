ab2053b806167d70c793333403a255d452c9144b
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 00:33:15 +0500

    fixed

--- src/Arcana.Service/Services/CourseCategories/ICourseCategoryService.cs
+++ src/Arcana.Service/Services/CourseCategories/ICourseCategoryService.cs
@@ -1,12 +1,13 @@
-﻿using Arcana.Service.Configurations;
+﻿using Arcana.Domain.Entities.CourseCategories;
+using Arcana.Service.Configurations;
 
 namespace Arcana.Service.Services.CourseCategories;
 
 public interface ICourseCategoryService
 {
-    ValueTask<CourseCategoryService> CreateAsync(CourseCategoryService courseCategory);
-    ValueTask<CourseCategoryService> UpdateAsync(long id, CourseCategoryService courseCategory);
-    ValueTask<CourseCategoryService> DeleteAsync(long id);
-    ValueTask<CourseCategoryService> GetByIdAsync(long id);
-    ValueTask<IEnumerable<CourseCategoryService>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+    ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory);
+    ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory);
+    ValueTask<CourseCategory> DeleteAsync(long id);
+    ValueTask<CourseCategory> GetByIdAsync(long id);
+    ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
 }
