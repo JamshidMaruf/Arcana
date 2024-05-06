218c96a0c84e07f6f2e6c02266f5907650066ed6
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 00:33:50 +0500

    fixed

--- src/Arcana.Service/Services/CourseCategories/CourseCategoryService.cs
+++ src/Arcana.Service/Services/CourseCategories/CourseCategoryService.cs
@@ -1,25 +1,31 @@
-﻿using Arcana.Service.Configurations;
+﻿using Arcana.Domain.Entities.CourseCategories;
+using Arcana.Service.Configurations;
 
 namespace Arcana.Service.Services.CourseCategories;
 
-public class CourseCategoryService() : ICourseCategory
+public class CourseCategoryService() : ICourseCategoryService
 {
-    public ValueTask<CourseCategoryService> CreateAsync(CourseCategoryService courseCategory)
+    public ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask<IEnumerable<CourseCategoryService>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    public ValueTask<CourseCategory> DeleteAsync(long id)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask<CourseCategoryService> GetByIdAsync(long id)
+    public ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask<CourseCategoryService> UpdateAsync(long id, CourseCategoryService courseCategory)
+    public ValueTask<CourseCategory> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory)
     {
         throw new NotImplementedException();
     }
