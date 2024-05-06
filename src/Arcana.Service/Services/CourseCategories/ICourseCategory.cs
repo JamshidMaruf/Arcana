4a3763a8a9d7811982df064d324d49bdc73e78a7
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 00:19:32 +0500

    Wrote Interface

---
+++ src/Arcana.Service/Services/CourseCategories/ICourseCategory.cs
@@ -0,0 +1,12 @@
+﻿using Arcana.Domain.Entities.CourseCategories;
+using Arcana.Service.Configurations;
+
+namespace Arcana.Service.Services.CourseCategories;
+
+public interface ICourseCategory
+{
+    ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory);
+    ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory);
+    ValueTask<CourseCategory> GetByIdAsync(long id);
+    ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
