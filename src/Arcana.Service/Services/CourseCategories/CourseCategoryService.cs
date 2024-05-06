0b01ebd16e079d39ed186fb593f5b011d3a0491a
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 02:00:54 +0500

    Wrote CourseCategoryService

--- src/Arcana.Service/Services/CourseCategories/CourseCategoryService.cs
+++ src/Arcana.Service/Services/CourseCategories/CourseCategoryService.cs
@@ -1,32 +1,90 @@
-﻿using Arcana.Domain.Entities.CourseCategories;
+﻿using Arcana.DataAccess.UnitOfWorks;
+using Arcana.Domain.Entities.CourseCategories;
 using Arcana.Service.Configurations;
+using Arcana.Service.Exceptions;
+using Arcana.Service.Extensions;
+using Arcana.Service.Helpers;
+using Microsoft.EntityFrameworkCore;
 
 namespace Arcana.Service.Services.CourseCategories;
 
-public class CourseCategoryService() : ICourseCategoryService
+public class CourseCategoryService(IUnitOfWork unitOfWork) : ICourseCategoryService
 {
-    public ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory)
+    public async ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory)
     {
-        throw new NotImplementedException();
+        await unitOfWork.BeginTransactionAsync();
+
+        var existCourseCategory = await unitOfWork.CourseCategories.SelectAsync(
+            cc => cc.Name.ToLower() == courseCategory.Name.ToLower() &&
+            !cc.IsDeleted);
+
+        if (existCourseCategory is not null)
+            throw new AlreadyExistException("Course Category is already exists");
+
+        courseCategory.CreatedByUserId = HttpContextHelper.UserId;
+
+        var created = await unitOfWork.CourseCategories.InsertAsync(courseCategory);
+        await unitOfWork.SaveAsync();
+
+        await unitOfWork.CommitTransactionAsync();
+
+        return created;
     }
 
-    public ValueTask<CourseCategory> DeleteAsync(long id)
+    public async ValueTask<bool> DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        await unitOfWork.BeginTransactionAsync();
+
+        var existCourseCategory = await unitOfWork.CourseCategories.SelectAsync(
+            cc => cc.Id == id &&
+            !cc.IsDeleted)
+            ?? throw new NotFoundException($"CourseCategory is not found with Id = {id}");
+
+        existCourseCategory.DeletedByUserId = HttpContextHelper.UserId;
+        await unitOfWork.CourseCategories.DeleteAsync(existCourseCategory);
+        await unitOfWork.SaveAsync();
+
+        await unitOfWork.CommitTransactionAsync();
+
+        return true;
     }
 
-    public ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    public async ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
     {
-        throw new NotImplementedException();
+        var CourseCategories = unitOfWork.CourseCategories
+            .SelectAsQueryable(expression: cc => !cc.IsDeleted, isTracked: false)
+            .OrderBy(filter);
+
+        if (!string.IsNullOrEmpty(search))
+            CourseCategories = CourseCategories.Where(cc =>
+               cc.Name.ToLower().Contains(search.ToLower()));
+
+        return await CourseCategories.ToPaginateAsQueryable(@params).ToListAsync();
     }
 
-    public ValueTask<CourseCategory> GetByIdAsync(long id)
+    public async ValueTask<CourseCategory> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        var existCourseCategory = await unitOfWork.CourseCategories.SelectAsync(cc => cc.Id == id && !cc.IsDeleted)
+            ?? throw new NotFoundException($"CourseCategory is not found with Id = {id}");
+
+        return existCourseCategory;
     }
 
-    public ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory)
+    public async ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory)
     {
-        throw new NotImplementedException();
+        await unitOfWork.BeginTransactionAsync();
+
+        var existCourseCategory = await unitOfWork.CourseCategories.SelectAsync(cc => cc.Id == id && !cc.IsDeleted)
+            ?? throw new NotFoundException($"CourseCategory is not found with Id = {id}");
+
+        existCourseCategory.Name = courseCategory.Name;
+        existCourseCategory.UpdatedByUserId = HttpContextHelper.UserId;
+
+        var updated = await unitOfWork.CourseCategories.UpdateAsync(existCourseCategory);
+        await unitOfWork.SaveAsync();
+
+        await unitOfWork.CommitTransactionAsync();
+
+        return updated;
     }
 }
