e31170a90e895a91af4e6abd63f2a40250e0862e
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 02:07:44 +0500

    Created class

---
+++ src/Arcana.Service/Services/CourseComments/CourseCommentService.cs
@@ -0,0 +1,33 @@
+﻿using Arcana.DataAccess.UnitOfWorks;
+using Arcana.Domain.Entities.CourseComments;
+using Arcana.Service.Configurations;
+
+namespace Arcana.Service.Services.CourseComments;
+
+public class CourseCommentService(IUnitOfWork unitOfWork) : ICourseCommentService
+{
+    public ValueTask<CourseComment> CreateAsync(CourseComment courseComment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<CourseComment>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseComment> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseComment> UpdateAsync(long id, CourseComment courseComment)
+    {
+        throw new NotImplementedException();
+    }
+}
