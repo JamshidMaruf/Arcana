365a9197e5ce37d15fd19641d7660e4b9b9ca83f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 00:32:46 +0500

    Renamed

---
+++ src/Arcana.Service/Services/CourseCategories/CourseCategoryService.cs
@@ -0,0 +1,26 @@
+﻿using Arcana.Service.Configurations;
+
+namespace Arcana.Service.Services.CourseCategories;
+
+public class CourseCategoryService() : ICourseCategory
+{
+    public ValueTask<CourseCategoryService> CreateAsync(CourseCategoryService courseCategory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<CourseCategoryService>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategoryService> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategoryService> UpdateAsync(long id, CourseCategoryService courseCategory)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Arcana.Service/Services/CourseCategories/ICourseCategoryService.cs
+++ src/Arcana.Service/Services/CourseCategories/ICourseCategoryService.cs
@@ -4,9 +4,9 @@ namespace Arcana.Service.Services.CourseCategories;
 
 public interface ICourseCategoryService
 {
-    ValueTask<CourseCategory> CreateAsync(CourseCategory courseCategory);
-    ValueTask<CourseCategory> UpdateAsync(long id, CourseCategory courseCategory);
-    ValueTask<CourseCategory> DeleteAsync(long id);
-    ValueTask<CourseCategory> GetByIdAsync(long id);
-    ValueTask<IEnumerable<CourseCategory>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+    ValueTask<CourseCategoryService> CreateAsync(CourseCategoryService courseCategory);
+    ValueTask<CourseCategoryService> UpdateAsync(long id, CourseCategoryService courseCategory);
+    ValueTask<CourseCategoryService> DeleteAsync(long id);
+    ValueTask<CourseCategoryService> GetByIdAsync(long id);
+    ValueTask<IEnumerable<CourseCategoryService>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
 }
