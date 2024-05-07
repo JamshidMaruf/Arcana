4641f05a4f7a76384f66e4579ed16433588f30cd
Faxrullo <faxrullofayzullayev@gmail.com>
2024-05-07 17:10:04 +0500

    Added instuctorStars and language Service and interface

--- src/Arcana.Service/Services/Languages/ILanguageService.cs
+++ src/Arcana.Service/Services/Languages/ILanguageService.cs
@@ -1,11 +1,6 @@
 using System.ComponentModel.DataAnnotations;
-using Arcana.DataAccess.UnitOfWorks;
 using Arcana.Domain.Entities.Languages;
 using Arcana.Service.Configurations;
-using Arcana.Service.Exceptions;
-using Arcana.Service.Extensions;
-using Arcana.Service.Helpers;
-using Microsoft.EntityFrameworkCore;
 
 namespace Arcana.Service.Services.Languages;
 
@@ -16,48 +11,4 @@ public interface ILanguageService
     ValueTask<bool> DeleteAsync(long id);
     ValueTask<Language> GetByIdAsync(long id);
     ValueTask<IEnumerable<Language>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
-}
-public class LanguageService(IUnitOfWork unitOfWork) : ILanguageService
-{
-    public async ValueTask<Language> CreateAsync(Language language)
-    {
-        var existLanguage = await unitOfWork.Languages.SelectAsync(lan => lan.Name == language.Name);
-        if (existLanguage is not null)
-            throw new AlreadyExistException($"This language already exist Name: {language.Name}");
-        
-        language.CreatedByUserId = HttpContextHelper.UserId;
-        var caratedLanguage = await unitOfWork.Languages.InsertAsync(language);
-
-        await unitOfWork.SaveAsync();
-
-        return caratedLanguage;
-    }
-    public async ValueTask<Language> UpdateAsync(long id, Language language)
-    {
-        var existLanguage = await unitOfWork.Languages.SelectAsync(lan =>lan.Id == id)
-            ??throw new NotFoundException($"Language is not found with Id: {id}");
-
-        existLanguage.Name = language.Name;
-        existLanguage.ShortName = language.ShortName;
-        existLanguage.CreatedByUserId = HttpContextHelper.UserId;
-
-        await unitOfWork.Languages.UpdateAsync(existLanguage);
-        await unitOfWork.SaveAsync();
-
-        return existLanguage;
-    }
-
-    public async ValueTask<bool> DeleteAsync(long id)
-    {
-        var existLanguage = await unitOfWork.Languages.SelectAsync(lan => lan.Id == id)
-            ??throw new NotFoundException($"Language is not found with Id: {id}");
-        
-        existLanguage.DeletedByUserId = HttpContextHelper.UserId;
-        await unitOfWork.Languages.DeleteAsync(existLanguage);
-
-        await unitOfWork.SaveAsync();
-
-        return true;
-    }
-
 }
