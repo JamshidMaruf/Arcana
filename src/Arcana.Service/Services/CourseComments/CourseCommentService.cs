875509bd07a4e1ba7200d3bd50272c3aa7fc0855
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 13:25:22 +0500

    Wrote CourseCommentService

--- src/Arcana.Service/Services/CourseComments/CourseCommentService.cs
+++ src/Arcana.Service/Services/CourseComments/CourseCommentService.cs
@@ -1,33 +1,96 @@
 ﻿using Arcana.DataAccess.UnitOfWorks;
 using Arcana.Domain.Entities.CourseComments;
 using Arcana.Service.Configurations;
+using Arcana.Service.Exceptions;
+using Arcana.Service.Extensions;
+using Arcana.Service.Helpers;
+using Microsoft.EntityFrameworkCore;
 
 namespace Arcana.Service.Services.CourseComments;
 
 public class CourseCommentService(IUnitOfWork unitOfWork) : ICourseCommentService
 {
-    public ValueTask<CourseComment> CreateAsync(CourseComment courseComment)
+    public async ValueTask<CourseComment> CreateAsync(CourseComment courseComment)
     {
-        throw new NotImplementedException();
+        await unitOfWork.BeginTransactionAsync();
+
+        var existCourse = await unitOfWork.Courses.SelectAsync(c => c.Id == courseComment.CourseId && !c.IsDeleted)
+            ?? throw new NotFoundException($"Course not found with Id = {courseComment.CourseId}");
+
+        var existStudent = await unitOfWork.Students.SelectAsync(s => s.Id == courseComment.StudentId && !s.IsDeleted)
+            ?? throw new NotFoundException($"Student not found with Id = {courseComment.StudentId}");
+
+        courseComment.CreatedByUserId = HttpContextHelper.UserId;
+        var created = await unitOfWork.CourseComments.InsertAsync(courseComment);
+        await unitOfWork.SaveAsync();
+
+        created.Course = existCourse;
+        created.Student = existStudent;
+
+        await unitOfWork.CommitTransactionAsync();
+
+        return created;
     }
 
-    public ValueTask<bool> DeleteAsync(long id)
+    public async ValueTask<bool> DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        var existCourseComment = await unitOfWork.CourseComments.SelectAsync(c => c.Id == id && !c.IsDeleted)
+            ?? throw new NotFoundException($"CourseComment not found with Id = {id}");
+
+        existCourseComment.DeletedByUserId = HttpContextHelper.UserId;
+        await unitOfWork.CourseComments.DeleteAsync(existCourseComment);
+        await unitOfWork.SaveAsync();
+
+        return true;
     }
 
-    public ValueTask<IEnumerable<CourseComment>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
+    public async ValueTask<IEnumerable<CourseComment>> GetAllAsync(PaginationParams @params, Filter filter, string search = null)
     {
-        throw new NotImplementedException();
+        var CourseComments = unitOfWork.CourseComments.SelectAsQueryable
+            (c => !c.IsDeleted, includes: ["Student", "Course"], isTracked: false)
+            .OrderBy(filter);
+
+        if (!string.IsNullOrEmpty(search))
+            CourseComments = CourseComments.Where(c =>
+               c.Content.ToLower().Contains(search.ToLower()));
+
+        return await CourseComments.ToPaginateAsQueryable(@params).ToListAsync();
     }
 
-    public ValueTask<CourseComment> GetByIdAsync(long id)
+    public async ValueTask<CourseComment> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        var existCourseComment = await unitOfWork.CourseComments.SelectAsync(c => c.Id == id && !c.IsDeleted,
+            includes: ["Student", "Course"])
+            ?? throw new NotFoundException($"CourseComment not found with Id = {id}");
+
+        return existCourseComment;
     }
 
-    public ValueTask<CourseComment> UpdateAsync(long id, CourseComment courseComment)
+    public async ValueTask<CourseComment> UpdateAsync(long id, CourseComment courseComment)
     {
-        throw new NotImplementedException();
+        await unitOfWork.BeginTransactionAsync();
+
+        var existCourse = await unitOfWork.Courses.SelectAsync(c => c.Id == courseComment.CourseId && !c.IsDeleted)
+            ?? throw new NotFoundException($"Course not found with Id = {courseComment.CourseId}");
+
+        var existStudent = await unitOfWork.Students.SelectAsync(s => s.Id == courseComment.StudentId && !s.IsDeleted)
+            ?? throw new NotFoundException($"Student not found with Id = {courseComment.StudentId}");
+
+        var existCourseComment = await unitOfWork.CourseComments.SelectAsync(c => c.Id == id && !c.IsDeleted)
+           ?? throw new NotFoundException($"CourseComment not found with Id = {id}");
+
+        existCourseComment.Course = existCourse;
+        existCourseComment.Student = existStudent;
+        existCourseComment.Content = courseComment.Content;
+        existCourseComment.CourseId = courseComment.CourseId;
+        existCourseComment.StudentId = courseComment.StudentId;
+        existCourseComment.UpdatedByUserId = HttpContextHelper.UserId;
+
+        var updated = await unitOfWork.CourseComments.UpdateAsync(existCourseComment);
+        await unitOfWork.SaveAsync();
+
+        await unitOfWork.CommitTransactionAsync();
+
+        return updated;
     }
 }
