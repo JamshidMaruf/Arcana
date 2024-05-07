4b9d1c4e9da4b9463972f62f9cac79ff0c768030
wonderboy1w3 <wonderboy1w3@gmail.com>
2024-05-07 21:13:40 +0500

    Added big services

--- src/Arcana.Domain/Entities/Quizzes/Quiz.cs
+++ src/Arcana.Domain/Entities/Quizzes/Quiz.cs
@@ -1,5 +1,6 @@
 ﻿using Arcana.Domain.Commons;
 using Arcana.Domain.Entities.Courses;
+using Arcana.Domain.Entities.Questions;
 
 namespace Arcana.Domain.Entities.Quizzes;
 
@@ -7,8 +8,7 @@ public class Quiz : Auditable
 {
     public string Name { get; set; }
     public int QuestionCount { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
     public long ModuleId { get; set; }
     public CourseModule Module { get; set; }
+    public ICollection<Question> Questions { get; set; }
 }
