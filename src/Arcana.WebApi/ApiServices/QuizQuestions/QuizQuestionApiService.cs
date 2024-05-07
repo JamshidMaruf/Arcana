3c3723f870fa78409222eea699748c1c3ad4e184
NematovaH <husnidabonu224@gmail.com>
2024-05-07 12:51:30 +0500

    Added QuizQuestion and Question ApiServices and validators

---
+++ src/Arcana.WebApi/ApiServices/QuizQuestions/QuizQuestionApiService.cs
@@ -0,0 +1,41 @@
+﻿using Arcana.Domain.Entities.QuizQuestions;
+using Arcana.Service.Configurations;
+using Arcana.Service.Services.QuizQuestions;
+using Arcana.WebApi.Models.QuizQuestions;
+using AutoMapper;
+
+namespace Arcana.WebApi.ApiServices.QuizQuestions;
+
+public class QuizQuestionApiService(IMapper mapper, IQuizQuestionService quizQuestionService) : IQuizQuestionApiService
+{
+    public async ValueTask<QuizQuestionViewModel> PostAsync(QuizQuestionCreateModel createModel)
+    {
+        var mappedQuizQuestion = mapper.Map<QuizQuestion>(createModel);
+        var createdQuizQuestion = await quizQuestionService.CreateAsync(mappedQuizQuestion);
+        return mapper.Map<QuizQuestionViewModel>(createdQuizQuestion);
+    }
+
+    public async ValueTask<QuizQuestionViewModel> PutAsync(long id, QuizQuestionUpdateModel updateModel)
+    {
+        var mappedQuizQuestion = mapper.Map<QuizQuestion>(updateModel);
+        var updatedQuizQuestion = await quizQuestionService.UpdateAsync(id, mappedQuizQuestion);
+        return mapper.Map<QuizQuestionViewModel>(updatedQuizQuestion);
+    }
+
+    public async ValueTask<bool> DeleteAsync(long id)
+    {
+        return await quizQuestionService.DeleteAsync(id);
+    }
+
+    public async ValueTask<QuizQuestionViewModel> GetAsync(long id)
+    {
+        var existQuizQuestion = await quizQuestionService.GetByIdAsync(id);
+        return mapper.Map<QuizQuestionViewModel>(existQuizQuestion);
+    }
+
+    public async ValueTask<IEnumerable<QuizQuestionViewModel>> GetAsync(PaginationParams @params, Filter filter, string search = null)
+    {
+        var quizQuestions = await quizQuestionService.GetAllAsync(@params, filter, search);
+        return mapper.Map<IEnumerable<QuizQuestionViewModel>>(quizQuestions);
+    }
+}
