8e28f9bd935b16ee9014856cccb739cbc80e8b78
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 17:02:54 +0500

    created interface

---
+++ src/Arcana.WebApi/ApiServices/CourseCategories/CourseCategoryApiService.cs
@@ -0,0 +1,32 @@
+﻿using Arcana.Service.Configurations;
+using Arcana.WebApi.Models.CourseCategories;
+
+namespace Arcana.WebApi.ApiServices.CourseCategories;
+
+public class CourseCategoryApiService : ICourseCategoryApiService
+{
+    public ValueTask<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategoryViewModel> GetAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<CourseCategoryViewModel>> GetAsync(PaginationParams @params, Filter filter, string search = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategoryViewModel> PostAsync(CourseCategoryCreateModel createModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<CourseCategoryViewModel> PutAsync(long id, CourseCategoryUpdateModel updateModel)
+    {
+        throw new NotImplementedException();
+    }
+}
