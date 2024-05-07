bfdcaeea83dba1b148bcf7b1b606660fbf3245b2
NematovaH <husnidabonu224@gmail.com>
2024-05-07 19:25:18 +0500

    Added QuizQuestion validators

---
+++ src/Arcana.WebApi/Validators/QuizQuestions/QuizQuestionCreateModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using Arcana.WebApi.Models.QuizQuestions;
+using FluentValidation;
+
+namespace Arcana.WebApi.Validators.QuizQuestions;
+
+
+public class QuizQuestionCreateModelValidator : AbstractValidator<QuizQuestionCreateModel>
+{
+    public QuizQuestionCreateModelValidator()
+    {
+        RuleFor(quizQuestion => quizQuestion.QuestionId)
+            .NotNull()
+            .WithMessage(quizQuestion => $"{nameof(quizQuestion.QuestionId)} is not specified");
+
+        RuleFor(quizQuestion => quizQuestion.QuizId)
+            .NotNull()
+            .WithMessage(quizQuestion => $"{nameof(quizQuestion.QuizId)} is not specified");
+    }
+}
