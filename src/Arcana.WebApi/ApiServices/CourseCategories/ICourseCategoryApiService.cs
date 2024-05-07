cb88d2ce916ab6cf1229b2a9db2595fa01c1e075
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 17:02:00 +0500

    Wrote Interface

---
+++ src/Arcana.WebApi/ApiServices/CourseCategories/ICourseCategoryApiService.cs
@@ -0,0 +1,13 @@
+﻿using Arcana.Service.Configurations;
+using Arcana.WebApi.Models.CourseCategories;
+
+namespace Arcana.WebApi.ApiServices.CourseCategories;
+
+public interface ICourseCategoryApiService
+{
+    ValueTask<CourseCategoryViewModel> PostAsync(CourseCategoryCreateModel createModel);
+    ValueTask<CourseCategoryViewModel> PutAsync(long id, CourseCategoryUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<CourseCategoryViewModel> GetAsync(long id);
+    ValueTask<IEnumerable<CourseCategoryViewModel>> GetAsync(PaginationParams @params, Filter filter, string search = null);
+}
