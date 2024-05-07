adf729202242056de83a8169905ca655df88ae5b
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-05-07 17:16:56 +0500

    Wrote CourseCommentCreateModelValidator

---
+++ src/Arcana.WebApi/Validators/CourseComments/CourseCommentCreateModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using Arcana.WebApi.Models.CourseComments;
+using FluentValidation;
+
+namespace Arcana.WebApi.Validators.CourseComments;
+
+public class CourseCommentCreateModelValidator : AbstractValidator<CourseCommentCreateModel>
+{
+    public CourseCommentCreateModelValidator()
+    {
+        RuleFor(CourseComment => CourseComment.Content)
+            .NotNull()
+            .WithMessage(CourseComment => $"{nameof(CourseComment.Content)} is not specified");
+    }
+}
