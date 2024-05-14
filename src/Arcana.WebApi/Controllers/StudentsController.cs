711315aaa7e19172e1420f9d333006141338408a
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-05-14 20:49:37 +0500

    Update StudentsController.cs

--- src/Arcana.WebApi/Controllers/StudentsController.cs
+++ src/Arcana.WebApi/Controllers/StudentsController.cs
@@ -66,7 +66,7 @@ public class StudentsController(IStudentApiService studentApiService) : BaseCont
         });
     }
 
-    [HttpPost("{id:long}/files/upload")]
+    [HttpPost("{id:long}/pictures/upload")]
     public async ValueTask<IActionResult> UploadPictureAsync(long id, IFormFile file)
     {
         return Ok(new Response
@@ -77,7 +77,7 @@ public class StudentsController(IStudentApiService studentApiService) : BaseCont
         });
     }
 
-    [HttpPost("{id:long}/files/delete")]
+    [HttpPost("{id:long}/pictures/delete")]
     public async ValueTask<IActionResult> DeletePictureAsync(long id)
     {
         return Ok(new Response
